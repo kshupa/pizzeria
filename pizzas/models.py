69da183f243ba7e9099bcc8385e83357155ec58f
christina <christina@christinas-iMac.local>
2020-11-23 17:06:31 -0600

    activating pizza and toppings model

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    """Pizza name."""
+    name = models.CharField(max_length=100)
+
+
+class Topping(models.Model):
+    """Pizza toppings."""
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
