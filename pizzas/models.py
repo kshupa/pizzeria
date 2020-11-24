5828f5c8661efeca003bb7fb32650e0e95424484
christina <christina@christinas-iMac.local>
2020-11-23 19:03:53 -0600

    fix name issues

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,8 +4,17 @@ class Pizza(models.Model):
     """Pizza name."""
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        """Returns pizza name."""
+        return self.name
+
 
 class Topping(models.Model):
     """Pizza toppings."""
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        """Returns pizza toppings."""
+        return self.name
